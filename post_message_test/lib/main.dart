b3332db1dc2adff2cbe2efe475df623ae4210780
Larry Stent <lawrencestent@gmail.com>
2025-01-10 10:02:29 +0200

    initial commit

---
+++ post_message_test/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'src/app.dart';
+import 'src/settings/settings_controller.dart';
+import 'src/settings/settings_service.dart';
+
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+final localhostServer = InAppLocalhostServer(documentRoot: 'assets');
+WebViewEnvironment? webViewEnvironment;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.windows) {
+    final availableVersion = await WebViewEnvironment.getAvailableVersion();
+    assert(availableVersion != null,
+        'Failed to find an installed WebView2 runtime or non-stable Microsoft Edge installation.');
+
+    webViewEnvironment = await WebViewEnvironment.create(
+        settings: WebViewEnvironmentSettings(userDataFolder: 'custom_path'));
+  }
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+  // Set up the SettingsController, which will glue user settings to multiple
+  // Flutter Widgets.
+  final settingsController = SettingsController(SettingsService());
+
+  // Load the user's preferred theme while the splash screen is displayed.
+  // This prevents a sudden theme change when the app is first displayed.
+  await settingsController.loadSettings();
+
+  // Run the app and pass in the SettingsController. The app listens to the
+  // SettingsController for changes, then passes it further down to the
+  // SettingsView.
+  runApp(MyApp(settingsController: settingsController));
+}
---
+++ post_message_test/lib/src/sample_feature/sample_item_list_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:post_message_test/src/sample_feature/chrome_webview';
+
+import '../settings/settings_view.dart';
+import 'sample_item.dart';
+import 'sample_item_details_view.dart';
+
+/// Displays a list of SampleItems.
+class SampleItemListView extends StatelessWidget {
+  const SampleItemListView({
+    super.key,
+    this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
+  });
+
+  static const routeName = '/';
+
+  final List<SampleItem> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Items'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              // Navigate to the settings page. If the user leaves and returns
+              // to the app after it has been killed while running in the
+              // background, the navigation stack is restored.
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          ),
+        ],
+      ),
+      body: ChromeSafariBrowserExampleScreen(),
+    );
+  }
+}
